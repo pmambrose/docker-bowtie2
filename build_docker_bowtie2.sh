f3826789e8ddef3c9e65ff757bbeddc3544bb066
pmaexps <153436620+pmaexps@users.noreply.github.com>
2023-12-22 20:21:53 +0530

    updated build sh file

--- build_docker_bowtie2.sh
+++ build_docker_bowtie2.sh
@@ -4,8 +4,10 @@ arch=$(uname -m)
 
 if [ "$arch" = "x86_64" ]; then
   echo "The architecture is amd64"
-elif [ "$arch" = "aarch64" ]; then
+  # Add intel docker build command here
+elif [ "$arch" = "aarch64" ] || [ "$arch" = "arm64" ]; then
   echo "The architecture is arm64"
+  # Add apple silicon docker build command here
 else
   echo "The architecture is not supported"
 fi
