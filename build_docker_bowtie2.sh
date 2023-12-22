73dee62e5c31451a4595b41b6bdc68636989ad97
pmaexps <153436620+pmaexps@users.noreply.github.com>
2023-12-22 20:56:00 +0530

    Added Apple Silicon docker build command

--- build_docker_bowtie2.sh
+++ build_docker_bowtie2.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+# Bind variable to detect machine architecture
 arch=$(uname -m)
 
+# Build AMD64 docker image if Intel/AMD x86 architecture is detected
 if [ "$arch" = "x86_64" ]; then
   echo "The architecture is amd64"
   # Add intel docker build command here
+
+# Build ARM64 docker image if Apple Silicon or other ARM archtitecture is detected
 elif [ "$arch" = "aarch64" ] || [ "$arch" = "arm64" ]; then
   echo "The architecture is arm64"
-  # Add apple silicon docker build command here
+  docker build -t bowtie2 ./bowtie2-arm64
+
+# Do not build if neither architecture is detected
 else
   echo "The architecture is not supported"
 fi
