2b0636740326e72aae93730dbec22d6f216e0ede
pmambrose <154608358+pmambrose@users.noreply.github.com>
2023-12-27 19:12:05 +0530

    Renamed container

--- build_docker_bowtie2.sh
+++ build_docker_bowtie2.sh
@@ -6,12 +6,12 @@ arch=$(uname -m)
 # Build AMD64 docker image if Intel/AMD x86 architecture is detected
 if [ "$arch" = "x86_64" ]; then
   echo "The architecture is amd64"
-  docker build -t bowtie2test ./bowtie2-amd64
+  docker build -t bowtie2docker ./bowtie2-amd64
 
 # Build ARM64 docker image if Apple Silicon or other ARM archtitecture is detected
 elif [ "$arch" = "aarch64" ] || [ "$arch" = "arm64" ]; then
   echo "The architecture is arm64"
-  docker build -t bowtie2test ./bowtie2-arm64
+  docker build -t bowtie2docker ./bowtie2-arm64
 
 # Do not build if neither architecture is detected
 else
